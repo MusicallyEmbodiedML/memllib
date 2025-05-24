ce3591635cdd001165fa9039bf34e3ae80e6ac2d
Andrea Martelloni <a.martelloni@sussex.ac.uk>
2025-05-24 15:55:09 +0000

    Fixed FlashFS

--- hardware/FlashFS.hpp
+++ hardware/FlashFS.hpp
@@ -1,3 +1,6 @@
+#ifndef FLASH_FS_HPP
+#define FLASH_FS_HPP
+
 #include <Arduino.h>
 #include <VFS.h>
 #include <LittleFS.h>
@@ -10,4 +13,11 @@ namespace FlashFS {
         VFS.root(LittleFS);
         Serial.println("LittleFS initialized.");
     }
-}
+
+    bool exists(const char* filename)
+    {
+        return LittleFS.exists(filename);
+    }
+} // namespace FlashFS
+
+#endif // FLASH_FS_HPP
