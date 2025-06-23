ec9565640df247a090b0d00602d24236b13d4262
Andrea Martelloni <a.martelloni@sussex.ac.uk>
2025-06-23 12:13:42 +1000

    Made loop in audio app virtual for overloading

--- audio/AudioAppBase.hpp
+++ audio/AudioAppBase.hpp
@@ -30,7 +30,7 @@ public:
     static stereosample_t audioCallback(const stereosample_t x);
     virtual void Setup(float sample_rate, std::shared_ptr<InterfaceBase> interface);
     virtual void ProcessParams(const std::vector<float>& params);
-    void loop();
+    virtual void loop();
 };
 
 #endif // __AUDIO_APP_BASE_HPP__
