4a9b50bccc09adf4b6473940006139b621d7880c
chriskiefer <chrisk13fer@gmail.com>
2025-08-01 15:23:45 +0100

    block fix

--- hardware/memlnaut/display/BlockSelectView.hpp
+++ hardware/memlnaut/display/BlockSelectView.hpp
@@ -15,12 +15,13 @@ public:
     void OnSetup() override {
         int idx=1;
         for(int i=0; i < 4; i++) {
-            for(int j=0; j < 2; i++) {
+            for(int j=0; j < 2; j++) {
 
                 auto button = std::make_shared<ButtonView>(String(idx), idx, TFT_BLUE);
                 rect bounds = { area.x + 10 + (i * 60), area.y + 10 + (j*60), 50, 50 }; // Example bounds
                 AddSubView(button, bounds);
                 buttons.push_back(button);
+                idx++;
             }
         }
     }  
