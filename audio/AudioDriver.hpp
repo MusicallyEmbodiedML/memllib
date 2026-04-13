6bf7d2c362541047f02fdbf2f6dc359873bcd51f
chriskiefer <chrisk13fer@gmail.com>
2026-04-13 16:55:28 +0100

    sample rate

--- audio/AudioDriver.hpp
+++ audio/AudioDriver.hpp
@@ -10,6 +10,7 @@ extern "C" {
 
 const size_t kBufferSize = AUDIO_BUFFER_FRAMES;
 constexpr size_t kSampleRate = 48000;
+// constexpr size_t kSampleRate = 24000;
 constexpr float kSampleRateRcpr = 1.0/kSampleRate;
 const size_t kNChannels = 2;
 
